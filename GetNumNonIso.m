844a94663916df5531c07c130de47d10ce73e302
EshedRam <52962342+EshedRam@users.noreply.github.com>
2021-09-14 20:11:41 +0300

    Update GetNumNonIso.m

--- GetNumNonIso.m
+++ GetNumNonIso.m
@@ -1,7 +1,12 @@
+/*****************************************/
+/* Target: Calcualte number of non-iso matrices       */
+/* By: Eshed Ram (eshed.ram@gmail.com)                */
+/* Update: 09/14/2021                                 */
+/*****************************************/
 function K = GetNumNonIso(kappa,gamma)
-assert(gamma>1 && gamma<4); % 2 or 3
+assert(gamma>1 && gamma<4); % support 2 or 3 only
 if (gamma == 2)
-    K = 0.5*(GetJ(kappa)+nchoosek(kappa+3,3));%-(kappa+1);
+    K = 0.5*(GetJ(kappa)+nchoosek(kappa+3,3));
 end
 if (gamma == 3)
     K = GetA(kappa)+GetB(kappa)+GetC(kappa)-(GetJ(kappa)+GetD(kappa));
