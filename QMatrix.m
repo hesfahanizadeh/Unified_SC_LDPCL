45dd23ed6ea6c481ae3527eb27891eaf82ec4405
EshedRam <52962342+EshedRam@users.noreply.github.com>
2021-09-14 20:14:30 +0300

    Update QMatrix.m

--- QMatrix.m
+++ QMatrix.m
@@ -1,3 +1,8 @@
+/*****************************************/
+/* Target: Generate Q matrix for local code construction*/
+/* By: Eshed Ram (eshed.ram@gmail.com)                */
+/* Update: 09/14/2021                                 */
+/*****************************************/
 function Q = QMatrix(l,r,i)
 if(i<0 || i>= l)
     error('Invalid value for i');
