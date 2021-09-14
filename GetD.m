452e237286c5a101dc9eefbe96605726daa2c3ea
EshedRam <52962342+EshedRam@users.noreply.github.com>
2021-09-14 20:08:03 +0300

    Update GetD.m

--- GetD.m
+++ GetD.m
@@ -1,3 +1,8 @@
+/*****************************************/
+/* Target: Calcualte D_\kappa for non-iso matrices enum for gamma=3*/
+/* By: Eshed Ram (eshed.ram@gmail.com)                */
+/* Update: 09/14/2021                                 */
+/*****************************************/
 function D = GetD(kappa)
 D = 0.5*(nchoosek(kappa+3,3)-GetJ(kappa));
 end
